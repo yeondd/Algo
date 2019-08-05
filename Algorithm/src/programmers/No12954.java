92a35671e7a8a021728e61b69691ca76c4d74ee7
yeondd <garnet_22@naver.com>
2019-08-05 14:14:39 +0900

    수정

--- Algorithm/src/programmers/No12954.java
+++ Algorithm/src/programmers/No12954.java
@@ -7,16 +7,19 @@ package programmers;
 
 public class No12954 {
 	public static void main(String[] args) {
-		long[] answer = solution(-4, 2);
+		long[] answer = solution(10000000, 1000);
 		for(long i: answer) {
 			System.out.println(i);
 		}
 	}
 
 	public static long[] solution(int x, int n) {
-		long[] answer = new long[n];
+		long[] answer = {};
+		if(n > 1000) return answer;
+		else answer = new long[n];
 		
 		for(int i = 1; i <= n; i++) {
+			if(x > 10000000 || x < -10000000) {return new long[1];}
 			answer[i - 1] = x * i;
 		}
 		
