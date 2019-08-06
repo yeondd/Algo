abbd6cf72598ffd484f6d820bd393a1b236ae434
yeondd <garnet_22@naver.com>
2019-08-06 14:06:03 +0900

    정수 내림차순 배치. 여전히 long 범위때문에 실패. 범위를 벗어나는 경우 고려하지 않으면 런타임 에러, 고려하면 실패 뜸

--- Algorithm/src/programmers/No12933.java
+++ Algorithm/src/programmers/No12933.java
@@ -11,45 +11,51 @@ public class No12933 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		long n = 900000000;
-		long t = 1983619258; // 에러
-		long m = 999999999;
-		long k = 410065408;
+		// 2147483647 // 최대값
+		long n = 118372;
+//		long t = 1883619258; // 에러
+//		long m = 1883618258; // 에러
+		long m = 799999999;
+		long k = 2111111114;
+		long t = 2147483647; // 최대값
+//		long i = 2147483648; // 에러
 		System.out.println(solution(k));
 	}
 	
 	public static long solution(long n) {
 		long answer = 0;
-		// 1999999999
-		if(n > 2000000000) return answer;
+		String answerStr = "";
 		
-		// 이거 많이 쓰길래 나도 써봄.
-		// Returns the string representation of the long argument.
+		// 주어진 숫자를 string 배열로 바꾸고 정렬
 		String s = String.valueOf(n);
-		int nLength = s.length();
-		char[] nString = new char[nLength];
+		int strSize = s.length();
+		String[] chr = s.split("");
+		Arrays.sort(chr);
 		
-		// 정렬을 위해 s를 배열로
-		for(int i = 0; i < nLength; i++) {
-			nString[i] = s.charAt(i);
+		if(strSize >= 10) {
+			// 전체 배열에 있는 숫자 중 최대 값은 2이고, 한 개만 있어야 함.
+			if(chr[strSize - 1].compareTo("2") < 0) {	// 최대 값이 2 이하
+				// 관계없음
+				System.out.println("최대 값이 2 이하");
+			} else if(chr[strSize - 1].compareTo("2") == 0) {	// 최대 값이  2 일때
+				if(chr[strSize - 2].compareTo("2") >= 0) {		// 그 다음으로 큰 값이 2보다 크거나 같으면 안됨.
+					System.out.println("최대 값이 2 이고, 그 다음으로 큰 값이 2 이상");
+					return 0;
+				} else {
+					System.out.println("최대 값이 2 이고, 그 다음으로 큰 값이 2 미만");
+				}
+				
+			} else {	// 최대 값이 2보다 큰 경우 -> 안됨
+				System.out.println("최대 값이 2보다 큼");
+				return 0;
+			}
 		}
 		
-		// 정렬 -> 역순
-		Arrays.sort(nString);
-		for(int i = nString.length - 1; i >= 0 ; i--) {
-			System.out.print(nString[i]);
+		for(String str: chr) {
+			answerStr = str + answerStr;
 		}
-		System.out.println();
-		
-		int multip = 1;
-		int tempNum = 0;
-		for(int i = 0; i < nString.length; i++) {
-			tempNum = (nString[i] - '0') * multip;
-			System.out.println("tempNum: " + tempNum);
-			answer += tempNum;
-			multip *= 10;
-		}
-		
-	    return answer;
+
+		answer = (long) Integer.parseInt(answerStr);
+		return answer;
 	}
 }
