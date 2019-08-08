141dbc5c95b7e5471de2afddf9f43607ed12acf2
yeondd <garnet_22@naver.com>
2019-08-08 13:13:14 +0900

    x만큼 간격이 있는 n개의 숫자. tc 13, 14통과. 속도문제였음

--- Algorithm/src/programmers/No12954.java
+++ Algorithm/src/programmers/No12954.java
@@ -7,20 +7,23 @@ package programmers;
 
 public class No12954 {
 	public static void main(String[] args) {
-		long[] answer = solution(10000000, 1000);
+		long[] answer = solution(-1, 10);
 		for(long i: answer) {
 			System.out.println(i);
 		}
+
+		System.out.println(answer.length);
 	}
 
 	public static long[] solution(int x, int n) {
 		long[] answer = {};
+		
+		if(x > 1000000 || x < -1000000) {return new long[1];}
 		if(n > 1000) return answer;
 		else answer = new long[n];
 		
-		for(int i = 1; i <= n; i++) {
-			if(x > 10000000 || x < -10000000) {return new long[1];}
-			answer[i - 1] = x * i;
+		for(int i = 0; i < n; i++) {
+			answer[i] = x * (i + 1);
 		}
 		
 	    return answer;
