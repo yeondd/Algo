47e43e94ebc823b6991c65b16a06353f1efdebe9
yeondd <garnet_22@naver.com>
2019-08-08 17:25:38 +0900

    정수 내림차순으로 배치하기. int -> long으로 바꾸는 메소드 사용

--- Algorithm/src/programmers/No12933.java
+++ Algorithm/src/programmers/No12933.java
@@ -16,10 +16,10 @@ public class No12933 {
 //		long t = 1883619258; // 에러
 //		long m = 1883618258; // 에러
 		long m = 799999999;
-		long k = 2111111114;
+		long k = 2111111110;
 		long t = 2147483647; // 최대값
 //		long i = 2147483648; // 에러
-		System.out.println(solution(k));
+		System.out.println(solution(t));
 	}
 	
 	public static long solution(long n) {
@@ -32,30 +32,11 @@ public class No12933 {
 		String[] chr = s.split("");
 		Arrays.sort(chr);
 		
-		if(strSize >= 10) {
-			// 전체 배열에 있는 숫자 중 최대 값은 2이고, 한 개만 있어야 함.
-			if(chr[strSize - 1].compareTo("2") < 0) {	// 최대 값이 2 이하
-				// 관계없음
-				System.out.println("최대 값이 2 이하");
-			} else if(chr[strSize - 1].compareTo("2") == 0) {	// 최대 값이  2 일때
-				if(chr[strSize - 2].compareTo("2") >= 0) {		// 그 다음으로 큰 값이 2보다 크거나 같으면 안됨.
-					System.out.println("최대 값이 2 이고, 그 다음으로 큰 값이 2 이상");
-					return 0;
-				} else {
-					System.out.println("최대 값이 2 이고, 그 다음으로 큰 값이 2 미만");
-				}
-				
-			} else {	// 최대 값이 2보다 큰 경우 -> 안됨
-				System.out.println("최대 값이 2보다 큼");
-				return 0;
-			}
-		}
-		
 		for(String str: chr) {
 			answerStr = str + answerStr;
 		}
 
-		answer = (long) Integer.parseInt(answerStr);
+		answer = Long.parseLong(answerStr);
 		return answer;
 	}
 }
