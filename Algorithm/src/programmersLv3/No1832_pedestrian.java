560bde2f5970e5d6e2f97e9f3fe9a9ddcb5d9c39
yeondd <garnet_22@naver.com>
2019-08-28 16:14:02 +0900

    검색했는데도 안된다....

--- Algorithm/src/programmersLv3/No1832_pedestrian.java
+++ Algorithm/src/programmersLv3/No1832_pedestrian.java
@@ -1,13 +1,18 @@
 package programmersLv3;
 
-import java.util.Stack;
+/*
+ * 보행자 천국
+ * https://programmers.co.kr/learn/courses/30/lessons/1832
+ * https://hanxxang.tistory.com/77
+ */
 
 public class No1832_pedestrian {
 	public static void main(String[] args) {
 		int m = 3;
 		int n = 6;
 //		int[][] cityMap = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
-		int[][] cityMap = {{0, 2, 0, 0, 0, 2}, {0, 0, 2, 0, 1, 0}, {1, 0, 0, 2, 2, 0}};
+//		int[][] cityMap = {{0, 2, 0, 0, 0, 2}, {0, 0, 2, 0, 1, 0}, {1, 0, 0, 2, 2, 0}};
+		int[][] cityMap = {{0, 0, 0, 0, 0, 2}, {0, 0, 0, 0, 0, 0}, {1, 0, 0, 0, 0, 0}};
 		System.out.println(solution(m, n, cityMap));
 	}
 	
@@ -15,33 +20,43 @@ public class No1832_pedestrian {
 	// 자동차는 오른쪽 또는 아래 방향으로 한 칸씩 이동 가능
 	public static int solution(int m, int n, int[][] cityMap) {
 		int answer = 0;
-		BinaryTree bt = new BinaryTree();
-		boolean direction = true; // right
-		// left node -> right
-		// right node -> down
+		int[][] resultArr = new int[m][n];
+		
+		for(int i = 0; i < n; i++) {
+			resultArr[0][i] = 1;
+		}
+		
+		for(int i = 0; i < m; i++) {
+			resultArr[i][0] = 1;
+		}
 		
 		for(int i = 0; i < m; i++) {
 			for(int j = 0; j < n; j++) {
-
-				if(cityMap[i][j] == 0) {
-					if (direction) {
-						if(cityMap[i][j + 1] != 1 && cityMap[i + 1][j] != 1) {
-							TreeNode left = bt.makeBT(null, new Node(i, j + 1), null);
-							TreeNode right = bt.makeBT(null, new Node(i + 1, j), null);
-							TreeNode tn = bt.makeBT(left, new Node(i, j), right);
-						}
-					} else {
-						if(cityMap[i + 1][j] != 1 && cityMap[i][j + 1] != 1) {
-							TreeNode left = bt.makeBT(null, new Node(i, j + 1), null);
-							TreeNode right = bt.makeBT(null, new Node(i + 1, j), null);
-							TreeNode tn = bt.makeBT(left, new Node(i, j), right);
-						}
-					}	
+				
+				switch(cityMap[i][j]) {
+				
+				case 0:
+					if(i > 0 && j > 0) 
+						resultArr[i][j] = (resultArr[i - 1][j] % MOD + resultArr[i][j - 1] % MOD) % MOD;
+					break;
+					
+				case 1:
+					resultArr[i][j] = 0;
+					break;
+					
+				case 2: 
+					if(i > 0 && j > 0) 
+						resultArr[i][j] = (resultArr[i - 1][j] % MOD + resultArr[i][j - 1] % MOD) % MOD;
+					
+					if(resultArr[i][j] > 0) {
+						resultArr[i][j] = (resultArr[i][j] - 1) % MOD;
+					}
+					break;
 				}
 			}
 		}
 		
-		answer = answer % MOD;
+		answer = resultArr[m-1][n-1] % MOD;
 	    return answer;
 	}
 }
