b23374f9e79d81e583dea44f448c479279962151
yeondd <garnet_22@naver.com>
2019-08-29 15:28:39 +0900

    다리를 지나는 트럭 재시도

--- Algorithm/src/programmersLv2/No42583.java
+++ Algorithm/src/programmersLv2/No42583.java
@@ -1,109 +1,85 @@
 package programmersLv2;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/*
+ * 다리를 지나는 트럭
+ * https://programmers.co.kr/learn/courses/30/lessons/42583
+ */
 
 class Truck {
-	private int truck_weights;
-	private int len;
+	int weight;
+	int position;
 	
-	Truck() {
-	}
-	
-	Truck(int truck_weights) {
-		this.truck_weights = truck_weights;
-		this.len = 0;
+	public Truck() {}
+	public Truck(int weight) {
+		this.weight = weight;
+		position = 0;
 	}
 	
-	public void go1Step() {
-		len++;
+	public void going() {
+		position++;
 	}
 	
-	public boolean checkLen(int bridge_length) {
-		return (len == bridge_length) ? true : false;
+	public int getPosition() {
+		return position;
 	}
-
-	public int getTruck_weights() {
-		return truck_weights;
-	}
-
-	public void setTruck_weights(int truck_weights) {
-		this.truck_weights = truck_weights;
-	}
-
-	public int getLen() {
-		return len;
-	}
-
-	public void setLen(int len) {
-		this.len = len;
-	}
-	
 }
 
-public class No42583 {
-
-	public static void main(String[] args) {
-		int bridge_length = 2;
-		int weight = 10;
-		int[] truck_weights = {7,4,5,6};
-		
-		System.out.println(solution(bridge_length, weight, truck_weights));
-	}
-
-	public static int solution(int bridge_length, int weight, int[] truck_weights) {
+class solutionClass {
+	public int solution(int bridge_length, int weight, int[] truck_weights) {
         int answer = 0;
-        ArrayList<Truck> queue = new ArrayList<Truck>();
-        ArrayList<Truck> crossing = new ArrayList<Truck>();
+        Queue<Truck> queue = new LinkedList<Truck>();
+        ArrayList<Truck> waiting = new ArrayList<Truck>();
+		Iterator<Truck> iter = waiting.iterator();
+        int weightSum = 0;
+        int watingIdx = 0;
         
         for(int i: truck_weights) {
-        	Truck truck = new Truck(i);
-        	queue.add(truck);
+        	Truck t = new Truck(i);
+        	queue.offer(t);
         }
         
-    	int len = 0;
-    	int idx = 0;
-        Truck temp = new Truck();
-    	
-        while(true) {
-        	if(queue.isEmpty() && crossing.isEmpty()) break;
-        	if(answer > 10) break;
-        	
-        	// 0. 가져오고
+        while(!queue.isEmpty()) {
         	answer++;
-            int totalWeight = 0;
-        	temp = queue.get(idx);
         	
-        	// 1. 무게 체크
-        	for(Truck t: crossing) {
-            	// 현재 다리 위에 있는 트럭의 전체 무게 구하기
-        		totalWeight += t.getTruck_weights();
-        	}
-        	System.out.println("현재 전체 무게: " + totalWeight);
-        	
-        	// 제한 무게보다 작을 때만 동작 -> 아니면 idx 그대로
-        	if(temp.getTruck_weights() + totalWeight <= weight) {
-        		// 올릴 수 있으면 한 칸 이동 후 건너는 배열에 추가
-            	System.out.println("추가 된 트럭 무게: " + temp.getTruck_weights());
-            	totalWeight += temp.getTruck_weights();
-            	temp.go1Step();
-            	crossing.add(temp);
-            	idx++;
-        	}
-        	else {	// 못 올렸으면 현재 다리에 있는 모든 트럭 한 칸 씩 이동
-        		for(Truck t: crossing) {
-        			if(t.getLen() <= bridge_length) {
-            			t.go1Step();
-                    	System.out.println("이동: " + t.getTruck_weights() + " -> " + t.getLen());
-                    	
-            		}
-        			else {
-        				System.out.println("건너고 있는 배열에서 제거");
-            			crossing.remove(t);
+        	// 다리 위에 더 올릴 수 없으면 기존 대기줄에 있는 트럭 이동
+        	if(weightSum + queue.peek().weight > weight) {
+        		while (iter.hasNext()) {
+        		    Truck t = iter.next();
+        		    
+        		    // 트럭이 끝까지 가지 않으면 한 칸 go
+        		    if(t.getPosition() < bridge_length) {
+            			t.going();
+        			}
+        		    // 트럭이 끝에 도달한 경우 대기줄에서 제거
+        		    else {
+        				waiting.remove(t);
         			}
         		}
+        	}
+        	else {
+        		Truck temp = queue.poll();
+        		waiting.add(temp);
+            	weightSum += temp.weight;
+        		watingIdx++;
         	}
         }
         
         return answer;
     }
+}
+public class No42583 {
+	
+	public static void main(String[] args) {
+		solutionClass sc = new solutionClass();
+		int bridge_length = 2;
+		int weight = 10;
+		int[] truck_weights = {7,4,5,6};
+		
+		System.out.println(sc.solution(bridge_length, weight, truck_weights));
+	}
 }
