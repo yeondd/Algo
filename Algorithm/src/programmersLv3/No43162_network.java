422742cc7faf5adbddce3c91ae52cb42ae1c02fd
yeondd <garnet_22@naver.com>
2019-08-30 15:30:55 +0900

    상하좌우 비교, 해당 노드 큐에 넣고 방문표시 하는 부분 함수로 분리

--- Algorithm/src/programmersLv3/No43162_network.java
+++ Algorithm/src/programmersLv3/No43162_network.java
@@ -11,6 +11,7 @@ import java.util.Queue;
 class Node {
 	int x;
 	int y;
+	LinkedList<Node> adjacent = new LinkedList<Node>();
 	
 	public Node() {}
 	public Node(int x, int y) {
@@ -22,26 +23,81 @@ public class No43162_network {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n = 3;
-//		int[][] computers = {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
-		int[][] computers = {{1, 1, 0}, {1, 1, 1}, {0, 1, 1}};
-		System.out.println(">>>> " + solution(n, computers));
+
+		int[][] computers1 = {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
+		System.out.println(">>>> " + solution(computers1.length, computers1));
+		
+		int[][] computers2 = {{1, 1, 0}, {1, 1, 1}, {0, 1, 1}};
+		System.out.println(">>>> " + solution(computers2.length, computers2));
+		
+		int[][] computers3 = {{1,0,0,1}, {0,1,1,1}, {0,1,1,0}, {1,1,0,1}};
+		System.out.println(">>>> " + solution(computers3.length, computers3) + "\n");
 	}
 
+	static boolean[][] visit;
+	static Queue<Node> queue;
+	static int[][] computer;
+	
+	public static void marked(int row, int col) {
+		System.out.printf("%d, %d\n", row, col);
+		queue.offer(new Node(row, col));
+		visit[row][col] = true;
+	}
+	
+	public static void compareAround(int row, int col, int n) {
+		int standard = computer[row][col];
+		// top
+		if(row - 1 >= 0 && standard == computer[row - 1][col] && !visit[row - 1][col]) {
+			marked(row - 1, col);
+		}
+		// right
+		if(col + 1 < n && standard == computer[row][col + 1] && !visit[row][col + 1]) {
+			marked(row, col + 1);
+		}
+		// down
+		if(row + 1 < n && standard == computer[row + 1][col] && !visit[row + 1][col]) {
+			marked(row + 1, col);
+		}
+		// left
+		if(col - 1 >= 0 && standard == computer[row][col - 1] && !visit[row][col - 1]) {
+			marked(row, col - 1);
+		}
+	}
+	
 	public static int solution(int n, int[][] computers) {
         int answer = 0;
-        boolean[][] visit = new boolean[n][n];
-        Queue<Node> queue = new LinkedList<Node>();
-        Node node = new Node();
-        
+        visit = new boolean[n][n];
+        queue = new LinkedList<Node>();
+        computer = computers;
+        Node node;
+       
         for(int i = 0; i < n; i++) {
         	for(int j = 0; j < n; j++) {
+        		// 이미 방문한 노드는 건너뛴다.
         		if(visit[i][j]) continue;
         		
+        		// queue에 아무것도 없다면 노드를 넣어야 함
         		if(queue.isEmpty()) {
-            		if(computers[i][j] == 1 && !visit[i][j]) {
-            			queue.offer(new Node(i, j));
-            			visit[i][j] = true;
+    				boolean conect = false;
+
+        			// 자신과 연결되어 있는 경우
+        			if(i == j) {
+        				// 자신과 연결된 노드가 하나도 없으면 queue에 넣고
+        				for(int k = 0; k < n; k++) {
+        					if(k != i && computer[i][k] == 1) {
+        						conect = true;
+        					} else if(k != i && computer[k][i] == 1) {
+        						conect = true;
+        					}
+        				}
+
+        				// 연결된 노드가 있으면 건너뜀
+        				if(conect) continue;
+        			}
+        			
+        			if(computer[i][j] == 1 && !visit[i][j]) {
+            			System.out.println("queue is empty");
+            			marked(i, j);
             			answer++;
             		}
         		}
@@ -51,26 +107,7 @@ public class No43162_network {
         			int row = node.x;
         			int col = node.y;
         			
-            		// top
-        			if(row - 1 >= 0 && computers[row][col] == computers[row - 1][col] && !visit[row - 1][col]) {
-        				queue.offer(new Node(row - 1, col));
-        				visit[row - 1][col] = true;
-        			}
-        			// right
-        			if(col + 1 < n && computers[row][col] == computers[row][col + 1] && !visit[row][col + 1]) {
-        				queue.offer(new Node(row, col + 1));
-        				visit[row][col + 1] = true;
-        			}
-        			// down
-        			if(row + 1 < n && computers[row][col] == computers[row + 1][col] && !visit[row + 1][col]) {
-        				queue.offer(new Node(row + 1, col));
-        				visit[row + 1][col] = true;
-        			}
-        			// left
-        			if(col - 1 >= 0 && computers[row][col] == computers[row][col - 1] && !visit[row][col - 1]) {
-        				queue.offer(new Node(row, col - 1));
-        				visit[row][col - 1] = true;
-        			}
+        			compareAround(row, col, n);
         		}
         	}
         }
