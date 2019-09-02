d851bb6c7a15c1f26a209b83276a6665aea601ce
yeondd <garnet_22@naver.com>
2019-09-02 17:54:56 +0900

    노드 접근 방식 변경. 하나의 노드랑 연결된 노드를 찾고, 방문으로 표시하면 일단 연결되어 있으므로 다음에 그 노드를 탐색하지 않아도 된다. visit 배열 확인

--- Algorithm/src/programmersLv3/No43162_network.java
+++ Algorithm/src/programmersLv3/No43162_network.java
@@ -27,111 +27,44 @@ public class No43162_network {
 //		int[][] computers1 = {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
 //		System.out.println(">>>> " + solution(computers1.length, computers1));
 //		
-//		int[][] computers2 = {{1, 1, 0}, {1, 1, 1}, {0, 1, 1}};
-//		System.out.println(">>>> " + solution(computers2.length, computers2));
+		int[][] computers2 = {{1, 1, 0}, {1, 1, 1}, {0, 1, 1}};
+		System.out.println(">>>> " + solution(computers2.length, computers2));
 //		
-//		int[][] computers3 = {{1,0,0,1}, {0,1,1,1}, {0,1,1,0}, {1,1,0,1}};
-//		System.out.println(">>>> " + solution(computers3.length, computers3) + "\n");
+		int[][] computers3 = {{1,0,0,1}, {0,1,1,1}, {0,1,1,0}, {1,1,0,1}};
+		System.out.println(">>>> " + solution(computers3.length, computers3) + "\n");
 		
 		int[][] computers4 = {{1,0,0,1,0,1,0}, {0,1,0,0,0,0,0}, {0,0,1,0,1,0,1}, {1,0,0,1,0,0,0}, {0,0,1,0,1,0,0}, {1,0,0,0,0,1,0}, {0,0,1,0,0,0,1}};
 		System.out.println(">>>> " + solution(computers4.length, computers4) + "\n");
 	}
 
-	static boolean[][] visit;
+	static boolean[] visit;
 	static Queue<Node> queue;
 	static int[][] computer;
 	
-	public static void marked(int row, int col) {
-		System.out.printf("%d, %d\n", row, col);
-		queue.offer(new Node(row, col));
-		visit[row][col] = true;
-	}
-	
-	public static void compareAround(int row, int col, int n) {
-		int standard = computer[row][col];
-		// top
-		if(row - 1 >= 0 && standard == computer[row - 1][col] && !visit[row - 1][col]) {
-			marked(row - 1, col);
-		}
-		// right
-		if(col + 1 < n && standard == computer[row][col + 1] && !visit[row][col + 1]) {
-			marked(row, col + 1);
-		}
-		// down
-		if(row + 1 < n && standard == computer[row + 1][col] && !visit[row + 1][col]) {
-			marked(row + 1, col);
-		}
-		// left
-		if(col - 1 >= 0 && standard == computer[row][col - 1] && !visit[row][col - 1]) {
-			marked(row, col - 1);
+	public static void compareAround(int row, int n) {
+		for(int i = 0; i < n; i++) {
+			if(!visit[i] && computer[row][i] == 1) {
+				visit[i] = true;
+            	System.out.println("\tvisited: " + i);
+				compareAround(i, n);
+			}
 		}
 	}
 	
 	public static int solution(int n, int[][] computers) {
         int answer = 0;
-        visit = new boolean[n][n];
+        visit = new boolean[n];
         queue = new LinkedList<Node>();
         computer = computers;
-        Node node;
-        Node[] nodes = new Node[n];
         
         for(int i = 0; i < n; i++) {
-        	nodes[i] = new Node(i, 0);
-        	nodes[i].adjacent.add();
-        }
-        
-        for(int i = 0; i < n; i++) {
-        	node = nodes[i];
-        	
-        	for(int j = i; j < n; j++) {
-        		if(computer[i][j] == 1) {
-        			Node conectedNode = nodes[j];
-        			if(node.adjacent.contains(conectedNode)) {
-        				node.adjacent.add(conectedNode);
-        			}
-        		}
+        	if(!visit[i]) {
+            	System.out.println(i + ") ");
+        		compareAround(i, n);
+        		answer++;
         	}
         }
        
-        for(int i = 0; i < n; i++) {
-        	for(int j = 0; j < n; j++) {
-        		// 이미 방문한 노드는 건너뛴다.
-        		if(visit[i][j]) continue;
-        		
-        		// queue에 아무것도 없다면 노드를 넣어야 함
-        		if(queue.isEmpty()) {
-    				boolean conect = false;
-
-        			// 자신과 연결되어 있는 경우
-        			if(i == j) {
-        				// 자신과 연결된 노드가 하나도 없으면 queue에 넣고
-        				for(int k = 0; k < n; k++) {
-        					if(k != i && computer[i][k] == 1) {
-        						conect = true;
-        					}
-        					else if(k != i && computer[k][i] == 1) {
-        						conect = true;
-        					}
-        				}
-
-        				// 연결된 노드가 있으면 건너뜀
-        				if(conect) continue;
-        			}
-        			
-        			if(computer[i][j] == 1 && !visit[i][j]) {
-            			System.out.println("queue is empty");
-            			marked(i, j);
-            			answer++;
-            		}
-        		}
-        		
-        		while(!queue.isEmpty()) {
-        			node = queue.poll();
-        			compareAround(node.x, node.y, n);
-        		}
-        	}
-        }
-        
         return answer;
     }
 }
