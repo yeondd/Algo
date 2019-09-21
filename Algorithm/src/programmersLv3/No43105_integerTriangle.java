c7882f36382db67f18f1b61a6d6d5a9781f595de
sy <>
2019-09-21 20:17:54 +0900

    (통과) node 대신 원본 triangle 배열에 바뀐 값 저장. 마지막 max값 구하려는데 Math.max()쓰니까 틀린 값이 나온다 왜지?

--- Algorithm/src/programmersLv3/No43105_integerTriangle.java
+++ Algorithm/src/programmersLv3/No43105_integerTriangle.java
@@ -1,31 +1,9 @@
 package programmersLv3;
 
-import java.util.LinkedList;
-
 /*
  * 정수 삼각형  
  * https://programmers.co.kr/learn/courses/30/lessons/43105
  */
-
-class Node {
-	int v;
-	int x;
-	int y;
-	LinkedList<Node> adjacent = new LinkedList<Node>();
-	
-	public Node() {}
-	public Node(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
-	
-
-	public Node(int v, int x, int y) {
-		this.v = v;
-		this.x = x;
-		this.y = y;
-	}
-}
 
 public class No43105_integerTriangle {
 
@@ -37,51 +15,35 @@ public class No43105_integerTriangle {
 	public static int solution(int[][] triangle) {
         int answer = 0;
         int height = triangle.length;
-        int maxNum = triangle[height - 1].length;
-        Node[][] nodeTriangle = new Node[height][maxNum];
         
         int value = triangle[0][0];
-        int left = triangle[0][0];
-        int right = triangle[0][0];
-        nodeTriangle[0][0] = new Node(value, left, right);
         
         for(int i = 1; i < height; i++) {
         	for(int j = 0; j <=  i; j++) {
         		value = triangle[i][j];
-//        		System.out.println("vlaue: " + value);
         		
         		if(j == 0) {
-        			right = nodeTriangle[i - 1][j].x + triangle[i][j];
-        			left = right;
-        		} 
+        			value += triangle[i - 1][j];
+        		}
         		else if(j == i) {
-            		left = nodeTriangle[i - 1][j - 1].x + triangle[i][j];
-            		right = left;
+        			value += triangle[i - 1][j - 1];
         		}
         		else {
-        			left = nodeTriangle[i - 1][j - 1].y + triangle[i][j];
-        			right = nodeTriangle[i - 1][j].x + triangle[i][j];
+        			int temp1 = value + triangle[i - 1][j - 1];
+        			int temp2 = value + triangle[i - 1][j];
+        			value = Math.max(temp1, temp2);
         		}
 
-//        		System.out.println("\tleft: " + left);
-//        		System.out.println("\tright: " + right);
-        		nodeTriangle[i][j] = new Node(value, left, right);
+        		triangle[i][j] = value;
+//        		System.out.println("vlaue: " + value);
         	}
-//    		System.out.println("==================");
         }
         
-        int maxPath = -1;
         for(int i = 0; i < height - 1; i++) {
-        	if(nodeTriangle[height - 1][i].x >= maxPath) {
-        		maxPath = nodeTriangle[height - 1][i].x;
-        	}
-        	
-        	if(nodeTriangle[height - 1][i].y >= maxPath) {
-        		maxPath = nodeTriangle[height - 1][i].y;
+        	if(triangle[height - 1][i] >= answer) {
+        		answer = triangle[height - 1][i];
         	}
         }
-        answer = maxPath;
-       
         return answer;
     }
 }
